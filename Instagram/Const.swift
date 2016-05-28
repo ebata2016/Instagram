ded4489154877701dae2c40846192e44b7e9dffb
江端正道 <ebata@me.com>
2016-05-28 20:39:08 +0900

    20160528

---
+++ Instagram/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Masamichi Ebata on 2016/05/28.
+//  Copyright © 2016年 masamichi.ebata. All rights reserved.
+//
+
+import Foundation
+
+struct CommonConst {
+    static let FirebaseURL = "https://amber-torch-1823.firebaseIO.com"
+    static let UsersPATH = "users"
+    static let PostPATH = "posts"
+    static let DisplayNameKey = "DISPLAYNAME"
+}
